22ea3d84c6312924c8f2a54011b614efa7b20f73
R1fty0 <113545024+R1fty0@users.noreply.github.com>
2024-05-23 14:44:53 -0700

    made player animaton controller, worked on syncing movement with animations, added third person camera controller

---
+++ Multiplayer FPS/scripts/animation_controller.gd
@@ -0,0 +1,30 @@
+extends Node3D
+class_name AnimationController
+
+@onready var anim_player = $"../AnimationPlayer"
+@onready var anim_state: AnimationState = AnimationState.IDLE
+
+enum AnimationState
+{
+	IDLE,
+	WALKING,
+	RUNNING,
+	JUMPING,
+	PUNCHING
+}
+
+func set_anim_state(new_state: AnimationState):
+	anim_state = new_state
+
+func _process(delta):
+	match anim_state:
+		AnimationState.IDLE:
+			anim_player.play("RobotArmature|Robot_Idle")
+		AnimationState.WALKING:
+			anim_player.play("RobotArmature|Robot_Walking")
+		AnimationState.RUNNING:
+			anim_player.play("RobotArmature|Robot_Running")			
+		AnimationState.JUMPING:
+			anim_player.play("RobotArmature|Robot_Jump")
+		AnimationState.PUNCHING:
+			anim_player.play("RobotArmature|Robot_Punch")
